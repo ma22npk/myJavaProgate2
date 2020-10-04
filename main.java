e67af6adf0a6a61560e6d95b59239090f72d60c5
Kota Matsumoto <k.matsumoto@blue-corporation.jp>
2020-10-04 13:14:33 +0900

    真偽知を出力する

---
+++ main.java
@@ -0,0 +1,18 @@
+class Main {
+  public static void main(String[] args) {
+    // 真偽値のデータ型はblooean型という。
+    // boolean型の変数定義
+    // boolean = データ型, bool = 変数名, true =値;
+    // boolean bool = true;
+
+    // 比較演算子(==)とは値を比較するための記号
+    // 6+2 == 5 ...false
+    // 6+2 != 5 ...true
+    // 6/3 == 2 ...true
+    // 6/2 != 2 ...false
+    System.out.println(true);
+    System.out.println(false);
+    System.out.println(6 + 2 == 5);
+    System.out.println(6 / 2 != 2);
+  }
+}
